f0573634bdea3c28ba8bcbf2799dfd2eac6fa192
cuixiaowu-y <15545644986@163.com>
2020-10-22 15:05:32 +0800

    空行

--- src/input/__test__/index.test.tsx
+++ src/input/__test__/index.test.tsx
@@ -7,8 +7,8 @@ import AweInput from "..";
 describe("Testing <AweInput />", () => {
   test("Show the maximum length and count ,uncontrolled", () => {
     render(<AweInput maxLength={10} showLengthCount />);
-    expect(screen.queryByText("5/10")).toBeNull();
 
+    expect(screen.queryByText("5/10")).toBeNull();
     fireEvent.change(screen.getByRole("textbox"), {
       target: { value: "hello" }
     });
@@ -19,6 +19,7 @@ describe("Testing <AweInput />", () => {
     render(
       <AweInput maxLength={10} showLengthCount defaultValue="我是默认的值！" />
     );
+
     expect(screen.queryByText("7/10")).toBeInTheDocument();
 
     // Restricted character
@@ -28,6 +29,7 @@ describe("Testing <AweInput />", () => {
 
   test("Show the maximum length and count ,controlled", () => {
     render(<AweInput maxLength={5} showLengthCount value="值" />);
+
     expect(screen.queryByText("1/5")).toBeInTheDocument();
     fireEvent.change(screen.getByRole("textbox"), {
       target: { value: "hello" }
@@ -37,6 +39,7 @@ describe("Testing <AweInput />", () => {
 
   test("Show the count ,controlled", () => {
     render(<AweInput showLengthCount value="值" />);
+
     expect(screen.queryByText("1")).toBeInTheDocument();
     fireEvent.change(screen.getByRole("textbox"), {
       target: { value: "hello" }
@@ -46,6 +49,7 @@ describe("Testing <AweInput />", () => {
 
   test("Don't show the count ,controlled", () => {
     render(<AweInput value="值" />);
+
     expect(screen.queryByText("1")).toBeNull();
     fireEvent.change(screen.getByRole("textbox"), {
       target: { value: "hello" }
