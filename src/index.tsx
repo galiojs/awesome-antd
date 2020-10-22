df462c0ecc880a6a110df58521f1d058603e07f4
Jerry W <wangguoguo.lan@gmail.com>
2020-10-22 15:32:58 +0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,3 +3,4 @@ export { default as AweSelect } from './select';
 export { default as AweApiSelect } from './api-select';
 export { default as AweCascader} from './cascader';
 export { default as AweApiCascader} from './api-cascader';
+export { default as AweInput } from './input';
