46039260acad8ad9228e976c1b2a6acf4829db5c
Jerry W <wangguoguo.lan@gmail.com>
2020-12-31 14:22:52 +0800

    fix: re-export all components from antd

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,5 @@
+export * from 'antd/lib';
+
 export { default as AweTreeSelect } from './tree-select';
 export { default as AweSelect } from './select';
 export { default as AweApiSelect } from './api-select';
